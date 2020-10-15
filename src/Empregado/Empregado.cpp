b69008d5fff4e6474ac2e848f4a3f76774e8bc5b
Luiz Almeida <luizorst@hotmail.com>
2020-10-15 16:53:12 -0300

    move method to cpp file

--- src/Empregado/Empregado.cpp
+++ src/Empregado/Empregado.cpp
@@ -8,3 +8,13 @@ double Empregado::getQuotaMensal() { return this->quotaMensalVendas; }
 void Empregado::setNome(std::string name) { this->nome = name; };
 void Empregado::setSalarioHora(float salary) { this->salarioHora = salary; }
 void Empregado::setQuotaMensalVendas(int vendas) { this->quotaMensalVendas = vendas; }
+double Empregado::pagamentoMes(double horasTrabalhadas) {
+    double t = horasTrabalhadas;
+    
+    //Cálculo de hora extra (+50% se horasTrabalhadas > 8)
+    if (horasTrabalhadas > 8) {
+        double x = horasTrabalhadas - 8;
+        t += x / 2;
+    }
+    return t * salarioHora;
+}
